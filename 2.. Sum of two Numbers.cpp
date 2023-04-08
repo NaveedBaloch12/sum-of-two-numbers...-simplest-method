a2147ca2a28b3d71413ef13bc4f18877e8d7055c
Naveed Hassan <120362779+NaveedBaloch12@users.noreply.github.com>
2023-04-08 21:14:03 +0500

    Add files via upload

---
+++ 2.. Sum of two Numbers.cpp
@@ -0,0 +1,16 @@
+//Adding of two numbers
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"This program will Add two numbers"<<endl;
+	int a,b,c;
+	cout<<"\aInput First Number= ";
+	cin>>a;
+	cout<<"Input Second Number= ";
+	cin>>b;
+	c=a+b;
+	cout<<"Sum of two numbers is= "<<c;
+}
